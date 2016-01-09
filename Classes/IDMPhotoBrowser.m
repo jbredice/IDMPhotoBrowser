b1a8a2b9135785a47e5b3b2464c724a7b7e2b2d1
Jason Bredice <jasonbredice@gmail.com>
2016-01-09 10:50:56 -0800

    dont close on action

--- Classes/IDMPhotoBrowser.m
+++ Classes/IDMPhotoBrowser.m
@@ -1260,7 +1260,6 @@ NSLocalizedStringFromTableInBundle((key), nil, [NSBundle bundleWithPath:[[NSBund
     
     if (_delegate && [_delegate respondsToSelector:@selector(actionButtonTapped:)]) {
         [_delegate performSelector:@selector(actionButtonTapped:) withObject:[photo underlyingImage]];
-        [self doneButtonPressed:sender];
         return;
     }
     
